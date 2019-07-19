69fab0d0c16a78b7d08b3a8be9e0c7eb6191be2f
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-07-19 16:23:23 +0300

    Cloud Streams: basic - publisher and subscriber

---
+++ Publisher/src/main/java/ru/shcheglov/cloud/TestChannel.java
@@ -0,0 +1,13 @@
+package ru.shcheglov.cloud;
+
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+public interface TestChannel {
+
+    String CHANNEL_NAME = "testChannel";
+
+    @Output(CHANNEL_NAME)
+    MessageChannel output();
+}
+
---
+++ Subscriber/src/main/java/ru/shcheglov/cloud/TestChannel.java
@@ -0,0 +1,13 @@
+package ru.shcheglov.cloud;
+
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.messaging.SubscribableChannel;
+
+public interface TestChannel {
+
+    String CHANNEL_NAME = "testChannel";
+
+    @Input(CHANNEL_NAME)
+    SubscribableChannel input();
+}
+
