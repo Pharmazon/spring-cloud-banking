9d5ea3512ceae8ae4b07093029e767ac53e6746b
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-07-19 13:14:07 +0300

    Cloud bus: push configurations

--- CardService/src/main/java/ru/shcheglov/cloud/CardRest.java
+++ CardService/src/main/java/ru/shcheglov/cloud/CardRest.java
@@ -1,12 +1,19 @@
 package ru.shcheglov.cloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class CardRest {
+
+    @Value("${test}")
+    private String test;
 
     @Autowired
     private CardNumberGenerator generator;
@@ -15,5 +22,10 @@ public class CardRest {
     @RequestMapping("create")
     public String createNewCard() {
         return generator.generate();
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return test;
     }
 }
